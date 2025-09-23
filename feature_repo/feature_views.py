954763c06ee5efbbab671d9e6af72f8f4124af7a
ROROROA <751502594@qq.com>
2025-09-23 21:06:27 +0800

    test

--- feature_repo/feature_views.py
+++ feature_repo/feature_views.py
@@ -61,22 +61,22 @@ user_rolling_features_view = FeatureView(
 
 # 特征视图 B: 电影的静态特征
 # 这个视图的数据源直接来自原始的 movies 表，因为电影的类型通常不会改变。
-# movie_static_features_source = FileSource(
-#     path="raw_data.movielens_movies", # 直接指向原始的 movies 表
-#     timestamp_field="created_timestamp", # 假设有一个电影条目创建时间戳
-#     description="A table containing static movie metadata.",
-# )
+movie_static_features_source = SparkSource(
+    path="raw_data.movielens_movies", # 直接指向原始的 movies 表
+    timestamp_field="created_timestamp", # 假设有一个电影条目创建时间戳
+    description="A table containing static movie metadata.",
+)
 
-# movie_static_features_view = FeatureView(
-#     name="movie_static_features",
-#     entities=[movie_entity],
-#     ttl=None,
-#     schema=[
-#         Field(name="title", dtype=String),
-#         Field(name="genres", dtype=String), # 电影类型，格式如 "Action|Adventure|Comedy"
-#     ],
-#     source=movie_static_features_source,
-#     online=True,
-#     tags={"group": "movie_static"},
-# )
+movie_static_features_view = FeatureView(
+    name="movie_static_features",
+    entities=[movie_entity],
+    ttl=None,
+    schema=[
+        Field(name="title", dtype=String),
+        Field(name="genres", dtype=String), # 电影类型，格式如 "Action|Adventure|Comedy"
+    ],
+    source=movie_static_features_source,
+    online=True,
+    tags={"group": "movie_static"},
+)
 
